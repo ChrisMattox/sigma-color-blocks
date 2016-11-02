2e4048d16e184f7c421f80c936729e24be4234f4
Kris Szafranski <kdszafranski@gmail.com>
2016-11-02 14:41:12 -0500

    working color blocks on the DOM

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-  console.log('here');
 
-  
+  var colors = ["red", "magenta", "cyan", "brown", "coral"];
+
+  for(var i = 0; i < colors.length; i++) {
+    $("#block-container").append('<div class="color-block"></div>');
+    var $el = $("#block-container").children().last();
+    $el.css("background-color", colors[i]);
+  }
+
+
 });
+
+
+// Utility functions
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
